1b5ac54a5ef6cab02fc5b103ff8f4bef3fb8230b
sfi <seb.figaro@gmail.com>
2021-03-03 14:50:25 +0100

    Déclaration d'une fonction

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
-function une_fonction()
-{
+function jsLoaded() {
     // chargement dans une variable de la balise <div id="jsLoaded">...</div>
     var jsloaded=document.querySelector("#jsLoaded");
 
     // Mise à jour du fond en vert
-    jsloaded.style.backgroundColor="green";
+    jsloaded.style.backgroundColor="rgba(153, 255, 0, 0.6)";
 
     // Mise à jour du texte affiché
     jsloaded.innerText="JS Chargé";
+
+    console.log("Chargemnt du JS terminé")
 }
 
-setTimeout(une_fonction, 3210)
-
-console.log("Chargemnt du JS terminé")
+setTimeout(jsLoaded, 3210)
